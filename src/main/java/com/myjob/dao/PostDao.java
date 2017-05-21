bce92d617edbfdc7fa417413a673301c0c839c33
RabinShrestha <lovingspirit617@gmail.com>
2017-05-21 14:51:45 -0500

    created PostDao, likeDao, CommentDao, and there Impl file

---
+++ src/main/java/com/myjob/dao/PostDao.java
@@ -0,0 +1,17 @@
+package com.myjob.dao;
+
+import com.myjob.model.Post;
+
+import java.util.List;
+
+/**
+ * Created by Rabin Shrestha on 5/21/2017.
+ */
+public interface PostDao {
+    Post get(Integer postId);
+    List<Post> getAll();
+    Post add(Post post);
+    Post update(Integer postId,Post post);
+    Post delete(Integer postId);
+
+}
---
+++ src/main/java/com/myjob/dao/impl/CommentDaoImpl.java
@@ -0,0 +1,36 @@
+package com.myjob.dao.impl;
+
+import com.myjob.dao.CommentDao;
+import com.myjob.model.Comment;
+
+import java.util.List;
+
+/**
+ * Created by Rabin Shrestha on 5/21/2017.
+ */
+public class CommentDaoImpl implements CommentDao {
+    @Override
+    public Comment get(Integer commentId) {
+        return null;
+    }
+
+    @Override
+    public List<Comment> getAll(Integer postId) {
+        return null;
+    }
+
+    @Override
+    public Comment update(Integer commentId, Comment comment) {
+        return null;
+    }
+
+    @Override
+    public Comment delete(Integer commentId) {
+        return null;
+    }
+
+    @Override
+    public Comment add(Integer postId, Comment comment) {
+        return null;
+    }
+}
