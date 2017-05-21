a440245e052884d9cc557990dce0b25e1c20e7df
manoj chaudhary <ctmanoj55@gmail.com>
2017-05-21 15:59:02 -0500

    Issue Fixed

--- src/main/java/com/myjob/dao/impl/LikeDaoImpl.java
+++ src/main/java/com/myjob/dao/impl/LikeDaoImpl.java
@@ -7,31 +7,20 @@ import com.myjob.model.Like;
  * Created by Rabin Shrestha on 5/21/2017.
  */
 public class LikeDaoImpl implements LikeDao {
-    // check if user have already liked the post or not
-
-    /**
-     *
-     * @param userId
-     * @param postId
-     * @return returns the LikeId if found the like
-     */
-    @Override
     public Integer getLikeStatus(Integer userId, Integer postId) {
         return null;
     }
 
-    @Override
     public Integer getNumberOfLikes(Integer postId) {
         return null;
     }
 
-    @Override
     public Like addLike(Integer userId, Integer postId) {
         return null;
     }
 
-    @Override
     public Like deleteLike(Integer userId, Integer postId) {
         return null;
     }
+    // check if user have already liked the post or not
 }
