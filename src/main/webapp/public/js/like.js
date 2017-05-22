51a8dcf445861f27ef0d346efc1df5b663ee5c03
Jamuna Ghale <zamuna16@gmail.com>
2017-05-22 17:33:04 -0500

    Altered pages

---
+++ src/main/webapp/public/js/like.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Zamuna on 5/22/2017.
+ */
+$(function () {
+    $("#commentId").hide();
+    $("#btnComment").click(function(){
+        $("#commentId").show();
+    });
+    $("#btnLike").click(function () {
+        alert("hello");
+    });
+
+    function handleLike() {
+    alert("hello");
+    }
+
+
+});
